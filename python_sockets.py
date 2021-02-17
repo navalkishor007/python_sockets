786bb7c261927b66f87666ffcd3dc65de728f178
navalkishor007 <67671688+navalkishor007@users.noreply.github.com>
2021-02-17 10:37:08 +0530

    created server socket to connect with the client compliet server code is here

--- python_sockets.py
+++ python_sockets.py
@@ -1,8 +1,12 @@
-class socket:
-    def __init__(self):
-        pass
-    def output(self):
-        print('working')
+from socket import *
 
-obj = socket()
-obj.output()
+def main():
+    host = gethostname()
+    port = 8088
+    s = socket()
+    s.bind((host,port))
+    print("Server is running.....")
+    s.listen(5)
+    c,add=s.accept()
+    print("Connection established")
+main()
